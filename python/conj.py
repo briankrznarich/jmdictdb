e0b48d0ac64a08db2adcf7774cd0fbb619507820
Stuart McGraw <stuart@soga.home>
2018-09-19 13:28:01 -0600

    Add redirection replacement for command line conj.py demo program
    python/conj.py was a command line program demonstating the use
    of the JMdictDB conjugation tables in a stand-alone, non-JMdictDB
    program.  It was extracted into an independent repository and
    removed from JMdictDB in jm:180610-1f524c. This commit restores
    a file of the same name that contains info pointing to the new
    location.

---
+++ python/conj.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+print ("""\
+
+conj.py, a table-driven Japanese verb and adjective conjugator
+utilizing the conjugation tables developed for JMdictDB, has 
+been forked to a separate project at:
+
+  https://gitlab.com/yamagoya/jconj
+
+The commit with its removal from JMdictDB is:
+
+  https://gitlab.com/yamagoya/jmdictdb/commit/1f524cdd
+""")
