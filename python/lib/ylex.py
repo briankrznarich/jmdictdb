7d348b811c95454f683dd98686857366500a665e
Stuart McGraw <stuart@soga.home>
2019-05-17 22:13:21 -0600

    ylex.py, yparse.py: update to Ply-3.11 base
    These files are the lexer and parser for parsing the YACC syntax
    used in jelparse.y.  They are modifications of the files of the
    same names shipped with Ply (third party parser-generator).  The
    modifications allow them to process the Python code snippets used
    in the jelparse.y action rules rather the the C code actions they
    were originally written for.
    
    This revision migrates those local changes from the old versions
    which were based on old, unknown versions of Ply (likely in the
    version 2.2 to 2.5 range) to the current Ply version 3.11 (although
    as it turns out there were very few non-whitespace upstream changes
    in that time).

--- python/lib/ylex.py
+++ python/lib/ylex.py
@@ -1,8 +1,13 @@
+# This file is a modified version of an example file provided with
+# Ply-3.11 (http://dabeaz.com/ply/index.html) in example/yply/ylex.py.
+# It has been modified for use with a corresponding yparse.y that has
+# bee modified to allow Python code (rather than C) in rule actions.
+# Stuart McGraw <jmdictdb@mtneva.com>
+
 # lexer for yacc-grammars
 #
 # Author: David Beazley (dave@dabeaz.com)
 # Date  : October 2, 2006
-
 
 import sys
 sys.path.append("../..")
@@ -10,106 +15,115 @@ sys.path.append("../..")
 from ply import *
 
 tokens = (
-    'LITERAL','SECTION','TOKEN','LEFT','RIGHT','PREC','START','TYPE','NONASSOC','UNION','CODE',
-    'ID','QLITERAL','NUMBER',
+    'LITERAL', 'SECTION', 'TOKEN', 'LEFT', 'RIGHT', 'PREC', 'START', 'TYPE', 'NONASSOC', 'UNION', 'CODE',
+    'ID', 'QLITERAL', 'NUMBER',
 )
 
-states = (('code','exclusive'),)
+states = (('code', 'exclusive'),)
 
-literals = [ ';', ',', '<', '>', '|',':' ]
+literals = [';', ',', '<', '>', '|', ':']
 t_ignore = ' \t'
 
-t_TOKEN     = r'%token'
-t_LEFT      = r'%left'
-t_RIGHT     = r'%right'
-t_NONASSOC  = r'%nonassoc'
-t_PREC      = r'%prec'
-t_START     = r'%start'
-t_TYPE      = r'%type'
-t_UNION     = r'%union'
-t_ID        = r'[a-zA-Z_][a-zA-Z_0-9]*'
+t_TOKEN = r'%token'
+t_LEFT = r'%left'
+t_RIGHT = r'%right'
+t_NONASSOC = r'%nonassoc'
+t_PREC = r'%prec'
+t_START = r'%start'
+t_TYPE = r'%type'
+t_UNION = r'%union'
+t_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
 t_QLITERAL  = r'''(?P<quote>['"]).*?(?P=quote)'''
-t_NUMBER    = r'\d+'
+t_NUMBER = r'\d+'
+
 
 def t_SECTION(t):
     r'%%'
-    if getattr(t.lexer,"lastsection",0):
-         t.value = t.lexer.lexdata[t.lexpos+2:]
-         t.lexer.lexpos = len(t.lexer.lexdata)
+    if getattr(t.lexer, "lastsection", 0):
+        t.value = t.lexer.lexdata[t.lexpos + 2:]
+        t.lexer.lexpos = len(t.lexer.lexdata)
     else:
-         t.lexer.lastsection = 0
+        t.lexer.lastsection = 0
     return t
 
 # Comments
+
+
 def t_ccomment(t):
     r'/\*(.|\n)*?\*/'
-    t.lineno += t.value.count('\n')
+    t.lexer.lineno += t.value.count('\n')
 
 t_ignore_cppcomment = r'//.*'
 
+
 def t_LITERAL(t):
-   r'%\{(.|\n)*?%\}'
-   t.lexer.lineno += t.value.count("\n")
-   s = t.value[2:-2]
-   if s.startswith(' '): s = s[1:]
-   t.value = s
-   return t
+    r'%\{(.|\n)*?%\}'
+    t.lexer.lineno += t.value.count("\n")
+    s = t.value[2:-2]
+    if s.startswith(' '): s = s[1:]
+    t.value = s
+    return t
+
 
 def t_NEWLINE(t):
-   r'\n'
-   t.lexer.lineno += 1
-   t.lexer.linestart = t.lexpos + 1
+    r'\n'
+    t.lexer.lineno += 1
+    t.lexer.linestart = t.lexpos + 1
 
 def t_code(t):
-   r'\{'
-   t.lexer.codestart = t.lexpos
-   t.lexer.level = 1
-   t.lexer.begin('code')
-   t.lexer.initindent = t.lexpos - t.lexer.linestart
+    r'\{'
+    t.lexer.codestart = t.lexpos
+    t.lexer.level = 1
+    t.lexer.begin('code')
+    t.lexer.initindent = t.lexpos - t.lexer.linestart
+
 
 def t_code_ignore_string(t):
-        r'(\"([^\\\n]|(\\.))*?\")|(\'([^\\\n]|(\\.))*?\')'
+    r'(\"([^\\\n]|(\\.))*?\")|(\'([^\\\n]|(\\.))*?\')'
+
 
 def t_code_ignore_char(t):
     r'\'([^\\\n]|(\\.))*?\''
 
+
 def t_code_ignore_comment(t):
-   r'\#(.|\n)*?'
+    r'\#(.|\n)*?'
+
+
+#def t_code_ignore_cppcom(t):
+#    r'//.*'
+
 
 def t_code_lbrace(t):
     r'\{'
     t.lexer.level += 1
 
+
 def t_code_rbrace(t):
     r'\}'
     t.lexer.level -= 1
     if t.lexer.level == 0:
-         t.type = 'CODE'
-         t.value = t.lexer.lexdata[t.lexer.codestart:t.lexpos+1]
-         t.lexer.begin('INITIAL')
-         t.lexer.lineno += t.value.count('\n')
-         return t
+        t.type = 'CODE'
+        t.value = t.lexer.lexdata[t.lexer.codestart:t.lexpos + 1]
+        t.lexer.begin('INITIAL')
+        t.lexer.lineno += t.value.count('\n')
+        return t
 
-t_code_ignore_nonspace   = r'[^\s\}\'\"\{]+'
+t_code_ignore_nonspace = r'[^\s\}\'\"\{]+'
 t_code_ignore_whitespace = r'\s+'
 t_code_ignore = ""
+
 
 def t_code_error(t):
     raise RuntimeError
 
+
 def t_error(t):
-    print ("%d: Illegal character '%s'" % (t.lineno, t.value[0]))
-    print (t.value)
+    print("%d: Illegal character '%s'" % (t.lexer.lineno, t.value[0]))
+    print(t.value)
     t.lexer.skip(1)
 
 lex.lex()
 
 if __name__ == '__main__':
     lex.runmain()
-
-
-
-
-
-
-
