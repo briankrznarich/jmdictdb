0c649f61acbdf7b40d384be3b12e74251566ffac
Stuart McGraw <stuart@soga.home>
2020-01-19 22:22:11 -0700

    entrs2xml.py: Remove multiple corpus capability
    Formerly the --corpora (-s) option of entrs2xml.py accepted
    multiple corpora.  This was originally intended to allow dumping
    an entire database of multiple corpora to XML using the extended
    dtd.  However, that ability has not been used, costed code
    complexity and has an alternative: dump corpora one at a time,
    all but the first without a dtd, and stitch the the files
    together manually.
    
    Other changes:
    - Remove --encoding option; encoding is now always utf-8.
    - Replace the --database, --host, --user, --port options with
      single --dburi option.
    
    Compared outputs of old and new versions of program on jmdict
    and jmnedict corpora in "compat" mode and got no differences.
    Did not check the extended output.

--- python/entrs2xml.py
+++ python/entrs2xml.py
@@ -34,7 +34,7 @@ def main (args, opts):
         Debug = opts.debug
           # Open the database.  jdb.dbopts() extracts the db-related
           # options from the command line options in 'opts'.
-        cur = jdb.dbOpen (opts.database, **jdb.dbopts (opts))
+        cur = jdb.dbOpen (None, **jdb.parse_pguri(opts.dburi))
 
           # If no "--root" option was supplied, choose a default based
           # on the value of the "--compat" option.
@@ -55,13 +55,11 @@ def main (args, opts):
             dir = jdb.find_in_syspath (dtd)
             dtdfn = dir + "/" + dtd             # Fully qualified dtd file name.
 
-              # jdb.get_dtd() reads the dtd text, and replaces the root
-              # element name name and encoding with the values supplied
-              # in the arguments.
-            dtdtxt= jdb.get_dtd (dtdfn, opts.root, opts.encoding)
+              # jdb.get_dtd() reads the dtd text and replaces the root
+              # element name name.
+            dtdtxt= jdb.get_dtd (dtdfn, opts.root)
             if len (args) == 0: outf = sys.stdout
             else: outf = open (args[0], "w")
-            jdb.reset_encoding (outf, opts.encoding)
             outf.write (dtdtxt)
 
         if opts.seqfile:
@@ -79,11 +77,6 @@ def main (args, opts):
                                % (opts.seqfile, lnnum, str(e)), file=sys.stderr)
             if f != sys.stdin: f.close()
 
-          # Turn the "--corpus" option value into a string that can be
-          # and'ed into a SQL WHERE clause to restrict the results to
-          # the specified corpora.
-        corp_terms = parse_corpus_opt (opts.corpus, 'e.src')
-
             # If the output file was not opened in the dtd section
             # above, open it now.  We postpose opening it until the
             # last possible moment to avoid creating it and then
@@ -95,44 +88,13 @@ def main (args, opts):
             if len (args) == 0: outf = sys.stdout
             else: outf = open (args[0], "w")
 
-        whr_act = " AND NOT unap AND stat="+str(jdb.KW.STAT['A'].id) if opts.compat else ""
-        if opts.begin:
-              # If a "--begin" sequence number was given, we need to read
-              # the entr record so we can get the src id number.  Complain
-              # and exit if not found.  Complain if more than one entry
-              # with the requested seq number exists.  More than one may be
-              # found since the same sequence number may exist in different
-              # corpora, or in the same corpus if an entry was edited.
-              #
-              #FIXME: no way to select from multiple entries with same seq
-              # number.  Might want just the stat="A" entries for example.
-            sql = "SELECT id,seq,src FROM entr e WHERE seq=%s%s%s ORDER BY src" \
-                    % (int(opts.begin), corp_terms, whr_act)
-            if Debug: print (sql, file=sys.stderr)
-            start = time.time()
-            rs = jdb.dbread (cur, sql)
-            if Debug: print ("Time: %s (init read)" % (time.time()-start), file=sys.stderr)
-            if not rs:
-                print ("No entry with seq '%s' found" \
-                                     % opts.begin, file=sys.stderr);  sys.exit (1)
-            if len(rs) > 1:
-                print ("Multiple entries having seq '%s' found, results " \
-                       "may not be as expected.  Consider using -s to " \
-                       "restrict to a single corpus." % (opts.begin), file=sys.stderr)
-            lastsrc, lastseq, lastid = rs[0].src, rs[0].seq, rs[0].id
-        if not opts.begin and not opts.seqfile:
-              # If no "--begin" option, remove the " AND" from the front of
-              # the 'corp_terms' string.  Read the first entry (by seq number)
-              # in the requested corpora.
-            cc = corp_terms[4:] if corp_terms else 'True'
-              # If compat (jmdict or jmnedict), restrict the xml to Active
-              # entries only.
-            sql = "SELECT id,seq,src FROM entr e WHERE %s%s ORDER BY src,seq LIMIT 1" % (cc, whr_act)
-            start = time.time()
-            if Debug: print (sql, file=sys.stderr)
-            rs = jdb.dbread (cur, sql)
-            if Debug: print ("Time: %s (init read)" % (time.time()-start), file=sys.stderr)
-            lastsrc, lastseq, lastid = rs[0].src, rs[0].seq, rs[0].id
+        corpid = opts.corpus
+        if corpid.isdigit(): corpid = int (corpid)
+        corpid = jdb.KW.SRC[corpid].id
+        whr_act = " AND src=%s" % corpid
+        if opts.compat:
+            whr_act += " AND NOT unap AND stat="+str(jdb.KW.STAT['A'].id)
+        lastsrc, lastseq, lastid = corpid, opts.begin or 0, 0
 
           # Add an enclosing root element only if we are also including
           # a DTD (ie, producing a full XML file).  Otherwise, the file
@@ -153,7 +115,7 @@ def main (args, opts):
                 if not seqnums: break
                 entrlist_loc += blksize
                   #FIXME: need detection of non-existent seq#s.
-                sql = "SELECT id FROM entr e WHERE seq IN %s" + corp_terms + whr_act
+                sql = "SELECT id FROM entr e WHERE seq IN %s" + whr_act
                 sql_args = [seqnums]
                 if Debug: print (sql, sql_args, file=sys.stderr)
                 start = time.time()
@@ -171,7 +133,7 @@ def main (args, opts):
 
                 whr = "WHERE ((e.src=%%s AND e.seq=%%s AND e.id>=%%s) " \
                               "OR (e.src=%%s AND e.seq>%%s) " \
-                              "OR e.src>%%s) %s%s" % (corp_terms, whr_act)
+                              "OR e.src>%%s) %s" % (whr_act,)
                 sql = "SELECT e.id FROM entr e" \
                       " %s ORDER BY src,seq,id LIMIT %d" \
                        % (whr, blksize if count is None else min (blksize, count))
@@ -230,26 +192,6 @@ def write_entrs (cur, entrs, raw, corpora, opts, outf):
             outf.write (txt + "\n")
         if Debug: print ("Time: %s (fmt)" % (time.time()-start), file=sys.stderr)
 
-def parse_corpus_opt (s, src_col):
-        if not s: return ''
-        in_srcs = [];  other_srcs = []; terms = []
-        terms = s.split(',')
-        for t in terms:
-            t1, x, t2 = t.partition (':')
-            if t1 and not t1.isdigit(): t1 = jdb.KW.SRC[t1].id
-            if t2 and not t2.isdigit(): t2 = jdb.KW.SRC[t2].id
-            if not x: in_srcs.append (str(int(t1)))
-            else:
-                if not t2: other_srcs.append ("%s>=%d" % (src_col, int(t1)))
-                elif not t1: other_srcs.append ("%s<=%d" % (src_col, int(t2)))
-                else: other_srcs.append ("%s BETWEEN %d AND %d" % (src_col, int(t1),int(t2)))
-        if in_srcs:
-            if len (in_srcs) == 1: other_srcs.append ("%s=%s" % (src_col, in_srcs[0]))
-            else: other_srcs.append ("%s IN(%s)" % (src_col, ",".join (in_srcs)))
-        clause = " AND ".join (other_srcs)
-        if clause: clause = " AND " + clause
-        return clause
-
 from optparse import OptionParser, OptionGroup
 from pylib.optparse_formatters import IndentedHelpFormatterWithNL
 
@@ -267,56 +209,10 @@ Arguments:
         p = OptionParser (usage=u, add_help_option=False,
                 formatter=IndentedHelpFormatterWithNL())
 
-        p.add_option ("--help",
-            action="help", help="Print this help message.")
-
-        p.add_option ("-b", "--begin", default=0,
-            type="int", metavar="SEQNUM",
-            help="Sequence number of first entry to process.  If not "
-                "given or 0, processing will start with the first entry.")
-
-        p.add_option ("-c", "--count", default=None,
-            type="int", metavar="NUM",
-            help="Number of entries to process.  If not given, "
-                "all entries in the file will be processed.")
-
-        p.add_option ("--seqfile", default=None,
-            help="Name of a file that contains a sequence numbers "
-                "to be extracted.  A line may contain multiple sequence "
-                "numbers separated by spaces.  A # character indicates a "
-                "comment and it and any text to the end of line will be "
-                "ignored, as will blank lines.  "
-                "--seqfile and the -b or -c options are mutually exclusive.")
-
-        p.add_option ("-s", "--corpus", default=None,
-            help="""Restrict extracted entries to those belonging to the
-                corpora defined by this option.  The format is a list of
-                comma separated specifiers.  Each specifier is either a
-                corpus id number, corpus name, or a range.  A range is a
-                corpus id number or corpus name followed by a colon, a
-                colon followed by a corpus id number or corpus name, or a
-                corpus id number or corpus name followed by a colon followed
-                by a corpus id number or corpus name.
-
-                Examples:
-
-                    -s 2         Restrict to corpus id 2 (jmnedict).
-
-                    -s jmnedict  Restrict to corpus id 2 (jmnedict).
-
-                    -s 3,26,27   Restrict to the corpora 3, 26, or 27.
-
-                    -s 10:13     Restrict to corpora 10, 11, 12, or 13.
-
-                    -s test:     Restrict to corpora with an id greater
-                                 or equal to the id of corpus "test".
-
-                    -s jmdict-examples,test
-                                 Restrict to corpora with id numbers
-                                 between "jmdict" and "examples", or equal
-                                 to the id of corpus "test".
-
-                Default is all corpora.""")
+        p.add_option ("-s", "--corpus", default="jmdict",
+            help="Extract entries belonging to this corpus.  May be "
+                "given as either a corpus name or id number.  Default is "
+                "\"jmdict\".")
 
         p.add_option ("--compat", default=None,
             help="""If given, COMPAT must have one of the following values:
@@ -342,6 +238,27 @@ Arguments:
                   describes the entry using an enhanced version
                   of the jmdict DTD.""")
 
+        p.add_option ("-b", "--begin", default=0,
+            type="int", metavar="SEQNUM",
+            help="Sequence number of first entry to process.  If not "
+                "given or 0, processing will start with the entry "
+                "with the smallest sequence number in the requested "
+                "corpus.  Mutually exclusive with the --seqfile option.")
+
+        p.add_option ("-c", "--count", default=None,
+            type="int", metavar="NUM",
+            help="Number of entries to process.  If not given, all "
+                "entries in the requested corpus will be processed.  "
+                "Mutually exclusive with the --seqfile option.  ")
+
+        p.add_option ("--seqfile", default=None,
+            help="Name of a file that contains a sequence numbers "
+                "to be processed.  A line may contain multiple sequence "
+                "numbers separated by spaces.  A # character indicates a "
+                "comment and it and any text to the end of line will be "
+                "ignored, as will blank lines.  Mutually exclusive with "
+                "the --begin or --count options.")
+
         p.add_option ("-r", "--root",
             help="""Name to use as the root element in the output XML file.
                 If 'compat' is None or "jmdict", default is "JMdict",
@@ -357,28 +274,11 @@ Arguments:
             help="Read and write entries in blocks of NUM entries.  "
                 "Default is 1000.")
 
-        p.add_option ("-e", "--encoding", default="utf-8",
-            help="Encoding for the output XML file.  Default is \"utf-8\".")
+        p.add_option ("-d", "--dburi", default="jmdict",
+            help="URI of the database to use.  Default is \"jmdict\".")
 
-        g = OptionGroup (p, "Database access options",
-                """The following options are used to connect to a
-                database in order to read the entries.
-
-                Caution: On many systems, command line option contents
-                may be visible to other users on the system.  For that
-                reason, you should avoid using the "--user" and "--password"
-                options below and use a .pgpass file (see the Postgresql
-                docs) instead. """)
-
-        g.add_option ("-d", "--database", default="jmdict",
-            help="Name of the database to load.  Default is \"jmdict\".")
-        g.add_option ("-h", "--host", default=None,
-            help="Name host machine database resides on.")
-        g.add_option ("-u", "--user", default=None,
-            help="Connect to database with this username.")
-        g.add_option ("-p", "--password", default=None,
-            help="Connect to database with this password.")
-        p.add_option_group (g)
+        p.add_option ("--help",
+            action="help", help="Print this help message.")
 
         p.add_option ("-D", "--debug", default="0",
             dest="debug", type="int",
