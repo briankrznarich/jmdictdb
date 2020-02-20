16195cbca62d7ece99161dfe6add6446c6328e99
Stuart McGraw <stuart@soga.home>
2020-02-20 13:48:11 -0700

    test_jdb_xref.py: refactor to use unittest.mock

--- python/tests/tests/test_jdb_xref.py
+++ python/tests/tests/test_jdb_xref.py
@@ -1,20 +1,41 @@
-import sys, unittest, pdb
+import sys, unittest, unittest.mock, pdb
 if '../lib' not in sys.path: sys.path.append ('../lib')
 from objects import *
 import jdb
 
-class Seq (unittest.TestCase):
+class Mark_seq_xrefs (unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        try: jdb.KW
+        except AttributeError: jdb.KW = jdb.Kwds (jdb.std_csv_dir())
+
+    # These are tests for the jdb.mark_seq_xref() function.
+    # Each test case creates a list of xrefs and calls jdb.mark_seq_xref()
+    # on them.  mark_seq_xrefs() searches the database for other entries
+    # having the same sequence number in order to determine its response.
+    # We provide a mock cursor for that database access.  The mock cursor
+    # arguments are a list of the rows that the simulated database returns
+    # which should be a list of 3-tuples: (src,seq,count) where 'count'
+    # is the number of active entries with that src,seq value.
+    # After calling mark_seq_xrefs() we use check_sql() to check that
+    # the mock cursor was called with the right sql and sql args.
+    # The check_sql() arguments are the sql args that were expected
+    # and are a list of: srcid, n*seq, KW.STAT['A'].id (=2).
+    # Following that we use unittest assert methods to check that
+    # mark_seq_xrefs() made the expected changes to the xrefs.
+
     def test_001(_):
-        xrefs = []
-        cur = MockCursor (_, None)
-        jdb.mark_seq_xrefs (cur, xrefs)
+        cur = MockCursor (None)
+        jdb.mark_seq_xrefs (cur, [])  # Empty list for no xrefs at all.
+        check_sql (cur, None)
 
     def test_002(_):
         e1 = Entr(id=200,src=1,seq=12345,stat=2,unap=False)
         x1 = Xref(entr=100,sens=1,typ=2,xentr=200,xsens=2)
         x1.TARG = e1;  x1._xsens = [2]
-        cur = MockCursor (_, [1,12345,2], [[e1.src,e1.seq,1]])
+        cur = MockCursor ([[e1.src,e1.seq,1]])
         jdb.mark_seq_xrefs (cur, [x1])
+        check_sql (cur, [1,12345,2])
         _.assertTrue (hasattr (x1, 'SEQ'))
         _.assertEqual (True, x1.SEQ)
 
@@ -25,8 +46,9 @@ class Seq (unittest.TestCase):
         x1.TARG = e1;  x1._xsens = [2,3]
         x2 = Xref(entr=100,sens=1,typ=2,xentr=200,xsens=3)
         x2.TARG = e1;  x2._xsens = []
-        cur = MockCursor (_, [1,12345,2], [[1,12345,1]])
+        cur = MockCursor ([[1,12345,1]])
         jdb.mark_seq_xrefs (cur, [x1,x2])
+        check_sql (cur, [1,12345,2])
         _.assertTrue (hasattr (x1, 'SEQ'))
         _.assertEqual (True, x1.SEQ)
         _.assertTrue (hasattr (x2, 'SEQ'))
@@ -40,8 +62,9 @@ class Seq (unittest.TestCase):
         x1.TARG = e1;  x1._xsens = [2]
         x2 = Xref(entr=100,sens=1,typ=2,xentr=201,xsens=3)
         x2.TARG = e2;  x2._xsens = [3]
-        cur = MockCursor (_, [1,12345,87654,2], [[1,12345,1],[1,87654,1]])
+        cur = MockCursor ([[1,12345,1],[1,87654,1]])
         jdb.mark_seq_xrefs (cur, [x1,x2])
+        check_sql (cur, [1,12345,87654,2])
           # We expect both xrefs to be tagged with a true SEQ value
           # even though they are for the same target seq because the
           # .xsens is different.  If the first has a ._xsens list,
@@ -60,8 +83,9 @@ class Seq (unittest.TestCase):
         x1.TARG = e1;  x1._xsens = [2]
         x2 = Xref(entr=100,sens=1,typ=2,xentr=201,xsens=2)
         x2.TARG = e2;  x2._xsens = [2]
-        cur = MockCursor (_, [1,12345,2], [[1,12345,2]])
+        cur = MockCursor ([[1,12345,2]])
         jdb.mark_seq_xrefs (cur, [x1,x2])
+        check_sql (cur, [1,12345,2])
           # Both xrefs of the single source entry point to target
           # entries of the same seq num, and all the entries if that
           # seq number are covered by the xrefs, so first xref gets
@@ -72,15 +96,17 @@ class Seq (unittest.TestCase):
         _.assertEqual (False, x2.SEQ)
 
     def test_006(_):
-          # 2 xrefs with different .sens -> 2 different targets w same seq number.
+          # 2 xrefs with different .sens -> 2 different targets with
+          # same seq number.
         e1 = Entr(id=200,src=1,seq=12345,stat=2,unap=False)
         e2 = Entr(id=201,src=1,seq=12345,stat=2,unap=True)
         x1 = Xref(entr=100,sens=1,typ=2,xentr=200,xsens=2)
         x1.TARG = e1;  x1._xsens = [2]
         x2 = Xref(entr=100,sens=1,typ=2,xentr=201,xsens=3)
         x2.TARG = e2;  x2._xsens = [3]
-        cur = MockCursor (_, [1,12345,2], [[1,12345,2]])
+        cur = MockCursor ([[1,12345,2]])
         jdb.mark_seq_xrefs (cur, [x1,x2])
+        check_sql (cur, [1,12345,2])
           # Both xrefs of the single source entry point to target
           # entries of the same seq num, and all the entries of that
           # seq number are covered by the xrefs, but source .xsens are
@@ -98,8 +124,9 @@ class Seq (unittest.TestCase):
         x1.TARG = e1;  x1._xsens = [2]
         x2 = Xref(entr=101,sens=1,typ=2,xentr=201,xsens=2)
         x2.TARG = e2;  x2._xsens = [2]
-        cur = MockCursor (_, [1,12345,2], [[1,12345,2]])
+        cur = MockCursor ([[1,12345,2]])
         jdb.mark_seq_xrefs (cur, [x1,x2])
+        check_sql (cur, [1,12345,2])
           # Expect no .SEQ attribute because there are two q=12345
           # entries, but each of the two source entry points to only
           # one of them, hence they cannot use a seq representation.
@@ -118,8 +145,9 @@ class Seq (unittest.TestCase):
         x2.TARG = e2;  x2._xsens = [2]
         x3 = Xref(entr=100,sens=1,typ=2,xentr=202,xsens=2)
         x3.TARG = e3;  x2._xsens = [2]
-        cur = MockCursor (_, [1,12345,2], [[1,12345,2]])
+        cur = MockCursor ([[1,12345,2]])
         jdb.mark_seq_xrefs (cur, [x1,x2,x3])
+        check_sql (cur, [1,12345,2])
           # Both xrefs of the single source entry point to target
           # entries of the same seq num, and all the entries of that
           # seq number are covered by the xrefs, so first xref gets
@@ -133,28 +161,23 @@ class Seq (unittest.TestCase):
 #=============================================================================
 # Support functions
 
-class MockCursor:
-    def __init__ (self, test, args=None, returns=None):
-          # test -- TestCase object (used for access to its asset* methods.
-          # args -- Arguments that are subsituted into a SQL statement that
-          #    this MockCursor instance will expect to be called with, and
-          #    which will result it a test fail if actual arguments are
-          #    different.
-          # returns -- Data that the database would have returned.
-        self.test = test; self.args = args; self.returns = returns
-        try: jdb.KW
-        except AttributeError: jdb.KW = jdb.Kwds (jdb.std_csv_dir())
-    def execute (self, sql, args=[]):
-          # If self.args is None, this method should never be called.
-        self.test.assertTrue (self.args is not None)
-        expect_sql = "SELECT src,seq,COUNT(*) FROM entr " \
-                        "WHERE src=%%s AND seq IN(%s) AND stat=%%s GROUP BY src,seq" \
-                        % ",".join(["%s"]*(len(args)-2))
-        self.test.assertEqual (expect_sql, sql)
-        self.test.assertEqual (self.args, args)
-    def fetchall (self):
-          # If self.args is None, this method should never be called.
-        self.test.assertTrue (self.args is not None)
-        return self.returns
+def MockCursor (rv):    # Create a mock database cursor.
+          # rv -- Rows expected to be returned from cursor.fetchall()
+        m = unittest.mock.Mock(['fetchall', 'execute'])
+        m.fetchall.return_value = rv
+        return m
+
+def check_sql (mock, sqlargs):
+          # Check that the database cursor (represented by 'mock') was
+          # asked to execute the expected SQL and args.
+        if not sqlargs:  # If no sqlargs, mark_seq_xrefs() wont use db.
+            mock.execute.assert_not_called()
+            mock.fetchall.assert_not_called()
+            return
+        sql = "SELECT src,seq,COUNT(*) FROM entr " \
+          "WHERE src=%%s AND seq IN(%s) AND stat=%%s GROUP BY src,seq" \
+           % ",".join(["%s"]*(len(sqlargs)-2))
+        mock.execute.assert_called_once_with (sql, sqlargs)
+        mock.fetchall.assert_called_once_with()
 
 if __name__ == '__main__': unittest.main()
