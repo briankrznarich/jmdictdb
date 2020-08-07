7652ac4587e767f35184c97590bf9ecefa4556ad
Stuart McGraw <stuart@soga.home>
2020-08-06 22:26:20 -0600

    db: various minor changes
    db/schema.sql: add progress messages.
    db/updates/033-042c98.sql: fix comment.
    db/updates/update.sh: quicky script for applying multiple
      updates.

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,17 @@
 
 \set ON_ERROR_STOP 1
 
+\qecho "Creating core JMdicDB database objects (db/mktables.sql)..."
 \ir mktables.sql
+
+\qecho "Loading the keyword tables (db/loadkw.sql)..."
 \ir loadkw.sql
+
+\qecho "Loading the conjugation tables (db/loadconj.sql)..."
 \ir loadconj.sql
+
+\qecho "Creating views (db/mkviews.sql)..."
 \ir mkviews.sql
+
+\qecho "Creating and loading the import schema (db/imptabs.sql)..."
 \ir imptabs.sql
