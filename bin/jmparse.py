briankrznarich/jmdictdb

commit 4bd2a13b53c278209e2405f22e81234816dd768a

jmparse.py, kdparse.py, exparse.py: updated for pgi.py changes
The jmparse.py, kdparse.py and exparse.py programs in bin/ were
updated to work with the new pgi.PgiWriter class.

Additional changes:
exparse.py:
- Renamed -t/--tempdir option to -T for consistency with other
  parser programs.
- Default -T/--tempdir option is now None (don't use temp files).
jmparse.py:
- Processing summary now output by logger rather than printing
  directly to stderr.
- Default -T/--tempdir option is now None (don't use temp files).
- Renamed -D/--debug option to -L/--level.
kdparse.py:
- Removed the -e/--encoding option.
- Removed the -t/--tempdir and -k/--keep options.  The kanjidic
  xml files are so small there is no reason to need external temp
  files.

bin/jmparse.py
@@ -11,7 +11,7 @@ from jmdictdb.pylib import progress_bar
 def main():
         global KW
         args, cfg = parse_cmdline(), {}
-        if args.debug: cfg['level'] = args.debug
+        if args.level: cfg['level'] = args.level
         if args.logfile:
             open (args.logfile, 'w').close()  # logger needs file to pre-exist.
             cfg['filename'] = args.logfile
@@ -42,7 +42,7 @@ def main():
         defcorp = xmltype or args.corpus
 
         inpf = jmxml.JmdictFile( open( args.filename ))
-        tmpfiles = pgi.initialize (args.tempdir)
+        pgio = pgi.PgiWriter (args.tempdir)
         eid = 0;  counts = collections.Counter()
         jmparser = jmxml.Jmparser (KW, xmltype)
         for typ, entr in jmparser.parse_file (
@@ -54,20 +54,20 @@ def main():
                 eid += 1; entr.idx = entr.id
                 if pbar: pbar (eid)
                 jdb.setkeys (entr, eid)
-                pgi.wrentr (entr, tmpfiles)
+                pgio.wrentr (entr)
                 counts[entr.src] += 1
             elif typ == 'grpdef':
-                pgi.wrgrpdef (entr, tmpfiles)
+                pgio.wrgrpdef (entr)
             elif typ == 'root':
                   # Note that 'entr' here is actually the tag name of the
                   # top-level element in the xml file, typically either
                   # "JMdict" or "JMnedict".
                 pass
         if args.progress: print()
-        pgi.wrcorpora (jmparser.corpora, defcorp, xmltype, tmpfiles)
-        pgi.finalize (tmpfiles, args.output, not args.keep)
+        pgio.wrcorpora (jmparser.corpora, defcorp, xmltype)
+        pgio.finalize (args.output, not args.keep)
         rpt = report_counts (counts, jmparser.corpora, defcorp)
-        print (rpt, file=sys.stderr)
+        L().sum(rpt)
 
 def parse_seq_opt (s):
         q = [int(x or 1) for x in s.split (',')]
@@ -155,12 +155,12 @@ def parse_cmdline():
                 "option has no effect on entries with a sequence number in "
                 "the XML.  ")
 
-        p.add_argument ("-k", "--keep", default=False, action="store_true",
-            help="Do not delete temporary files after program exits.")
-
         p.add_argument ("-l", "--logfile", default=None,
             help="Name of file to write log messages to.  If not given, "
                 "log mesages will be written stderr.")
+
+        p.add_argument ("-L", "--level", default="info",
+            help="Logging level: error, summary, warn, info, debug.")
 
         p.add_argument ("-p", "--progress", nargs='?',
                         default="percent", const=None,
@@ -172,16 +172,20 @@ def parse_cmdline():
                  Progress bar output is written to stderr."""\
                 .replace("\n"+(" "*16),''))  # See Note-1 below.
 
-        p.add_argument ("-T", "--tempdir", default=".",
-            help="Directory in which to create temporary files.")
+        p.add_argument ("-T", "--tempdir", default=None,
+            help="Directory in which to create temporary files.  "
+                "If not given temporary files will be buffered in memory "
+                "rather than on disk.  This option may be need on machines "
+                "with limited memory to force the use of disk.")
+
+        p.add_argument ("-k", "--keep", default=False, action="store_true",
+            help="Do not delete temporary files after program exits.  "
+                "Ignored if -T/--tmpdir is not also given.")
 
         p.add_argument ("-d", "--database",
             help="URI for a JMdictDB database to get tag infomation "
                 "from.  If not given, the standard built-in tags will "
                 "be used.")
-
-        p.add_argument ("-D", "--debug",
-            help="Debug level: warn, summary, info, debug.")
 
         # Note-1: Help text in triple quotes contains the embedded
         # leading space characters.  These are removed with the

bin/exparse.py
@@ -63,10 +63,7 @@ def main (args, opts):
 
         fin = ABPairReader (args[0])
           # FIXME: following gives localtime, change to utc or lt+tz.
-        mtime = datetime.date.fromtimestamp(os.stat(args[0])[8])
-        tmpfiles = pgi.initialize (opts.tempdir)
-        if not opts.noaction:
-            tmpfiles = pgi.initialize (opts.tempdir)
+        pgio = pgi.PgiWriter (opts.tempdir if not opts.noaction else None)
         srcid = 1    # This id, which will reference kwsrc.id after import,
                      # will be adjusted when data is imported into database
                      # so its value is arbitrary here.
@@ -74,14 +71,12 @@ def main (args, opts):
             entr.src = srcid
             if not opts.noaction:
                 jdb.setkeys (entr, eid+1)
-                pgi.wrentr (entr, tmpfiles)
+                pgio.wrentr (entr)
             if pbar: pbar (eid)
             if opts.count and eid+1 >= opts.count: break
-        srcrec = jdb.Obj (id=srcid, kw=opts.corpus, dt=mtime,
-                          seq='seq_'+opts.corpus, srct=KW.SRCT['examples'].kw)
-        pgi._wrrow (srcrec, tmpfiles['kwsrc'])
-        if not opts.noaction: pgi.finalize (tmpfiles, opts.output,
-                                            not opts.keep)
+        mtime = datetime.date.fromtimestamp(os.stat(args[0])[8]).isoformat()
+        pgio.wrcorpora ({opts.corpus:('examples',srcid)}, other={'dt':mtime})
+        if not opts.noaction: pgio.finalize (opts.output, not opts.keep)
 
 def parse_ex (fin, begin):
         # This is a generator function that will process one (A and B) pair
@@ -295,17 +290,19 @@ Arguments:
         p.add_option ("-s", "--corpus", default="examples",
             help='Corpus name to use for entries.  Default is "examples".')
 
-        p.add_option ("-k", "--keep", default=False,
-            dest="keep", action="store_true",
-            help="Do not delete temporary files after program exits.")
-
         p.add_option ("-l", "--logfile",
             dest="logfile", metavar="FILENAME",
             help="Name of file to write log messages to.")
 
-        p.add_option ("-t", "--tempdir", default=".",
-            dest="tempdir", metavar="DIRPATH",
-            help="Directory in which to create temporary files.")
+        p.add_option ("-T", "--tempdir", default=None,
+            help="Directory in which to create temporary files.  "
+                "If not given temporary files will be buffered in memory "
+                "rather than on disk.  This option may be need on machines "
+                "with limited memory to force the use of disk.")
+
+        p.add_option ("-k", "--keep", default=False, action="store_true",
+            help="Do not delete temporary files after program exits.  "
+                "Ignored if -T/--tmpdir is not also given.")
 
         p.add_option("--no-progress",
             dest="progbar", action="store_false", default=True,

bin/kdparse.py
@@ -45,9 +45,9 @@ def main (args, opts):
         global Lineno; Lineno = 1
         global KW
 
-        jdb.KW = KW = jdb.Kwds (jdb.std_csv_dir())
+        jdb.KW = KW = jdb.Kwds('')
 
-        if opts.l: opts.l = open (opts.l, "w", encoding=opts.e)
+        if opts.l: opts.l = open (opts.l, "w")
         else: opts.l = sys.stderr
         if not opts.o:
             fn = (os.path.split (args[0]))[1]
@@ -57,7 +57,7 @@ def main (args, opts):
             opts.o = None
         if opts.g: langs = [KW.LANG[iso639_1_to_2[x]].id for x in opts.g.split(',')]
         else: langs = None
-        workfiles = pgi.initialize (opts.t)
+        pgio = pgi.PgiWriter()
 
         pbar = None
         if opts.progbar:
@@ -70,11 +70,10 @@ def main (args, opts):
         srcid = 1    # This id, which will reference kwsrc.id after import,
                      # will be adjusted when data is imported into database
                      # so its value is arbitrary here.
-        srcdate = parse_xmlfile (args[0], srcid, workfiles, opts.b, opts.c, langs, pbar)
-        srcrec = jdb.Obj (id=srcid, kw=opts.corpus, dt=srcdate,
-                          seq='seq_'+opts.corpus, srct=KW.SRCT['kanjidic'].kw)
-        pgi._wrrow (srcrec, workfiles['kwsrc'])
-        pgi.finalize (workfiles, opts.o, not opts.k)
+        srcdate = parse_xmlfile (pgio, args[0], srcid,
+                                 opts.b, opts.c, langs, pbar)
+        pgio.wrcorpora ({opts.corpus:('kanjidic',srcid)}, other={'dt':srcdate})
+        pgio.finalize (opts.o)
         if not pbar: print ("\nDone!", file=sys.stderr)
 
 class LnFile:
@@ -86,7 +85,7 @@ class LnFile:
         s = self.source.readline();  self.lineno += 1
         return s
 
-def parse_xmlfile (infn, srcid, workfiles, start, count, langs, pbar):
+def parse_xmlfile (pgio, infn, srcid, start, count, langs, pbar):
 
         global Lineno
 
@@ -97,7 +96,7 @@ def parse_xmlfile (infn, srcid, workfiles, start, count, langs, pbar):
         # of the entry, and then write_entry() to do the actual
         # writing to the database.
 
-        inpf = LnFile( open (infn, encoding='utf-8'))
+        inpf = LnFile( open (infn))
         context = iter(ElementTree.iterparse( inpf, ("start","end")))
         event, root = next(context)
         if start and start>1: print ("Skipping initial entries...", file=sys.stderr)
@@ -153,7 +152,7 @@ def parse_xmlfile (infn, srcid, workfiles, start, count, langs, pbar):
 
                 entr = do_chr (elem, srcid, langs)
                 jdb.setkeys (entr, cntr)
-                pgi.wrentr (entr, workfiles)
+                pgio.wrentr (entr)
 
                 # Update the progress bar (if using one.)
                 if pbar: pbar (cntr)
@@ -429,21 +428,13 @@ arguments:
         p.add_option("--no-progress",
             dest="progbar", action="store_false", default=True,
             help="Don't show the progress bar.")
-        p.add_option ("-e", "--encoding",
-             type="str", dest="e", default="utf-8",
-             help="If --logfile given, write to it using this encoding.")
-        p.add_option ("-k", "--keep",
-             action="store_true", dest="k", default=False,
-             help="Do not delete the workfiles when finished.  This "
-                "can be useful for debugging.")
-        p.add_option ("-t", "--tempdir",
-             type="str", dest="t", default=".",
-             help="Create the work files in this directory.")
         opts, args = p.parse_args ()
-        if len(args) < 1: p.error("Too few arguments, expected name of kanjidic xml file."
-                                "\nUse --help for more info")
-        if len(args) > 1: p.error("Too many arguments, expected only name of kanjidic xml file"
-                                "\nUse --help for more info")
+        if len(args) < 1:
+            p.error("Too few arguments, expected name of kanjidic xml file."
+                    "\nUse --help for more info")
+        if len(args) > 1:
+            p.error("Too many arguments, expected only name of kanjidic xml"
+                    " file\nUse --help for more info")
         return args, opts
 
 if __name__ == '__main__':

commit 08532db230b92e0123f0a3c56491dcade6e81388

Tests: fix test_jmparse.py
The changes made to jmparse.py in 200830-4bd2a13 broke the jmparse
test which is fixed here.  Also changed the default logging level
in jmparse.py from "info" to "warn" because the former is too noisy.

bin/jmparse.py
@@ -159,8 +159,9 @@ def parse_cmdline():
             help="Name of file to write log messages to.  If not given, "
                 "log mesages will be written stderr.")
 
-        p.add_argument ("-L", "--level", default="info",
-            help="Logging level: error, summary, warn, info, debug.")
+        p.add_argument ("-L", "--level", default="warn",
+            help="Logging level: error, summary, warn, info, debug.  "
+                "Default is \"warn\".")
 
         p.add_argument ("-p", "--progress", nargs='?',
                         default="percent", const=None,

tests/data/jmparse/test001.log
@@ -1,0 +1,1 @@
+000000-000000-0000 S : Parsed 13 entries (*jmdict:13)

tests/tests/test_jmparse.py
@@ -1,5 +1,5 @@
 import sys, os, tempfile, atexit, subprocess, difflib, \
-        shutil, pdb, unittest, unittest_extensions
+        re, shutil, pdb, unittest, unittest_extensions
 from jmdictdb import jdb
 
 Global_setup_done = False
@@ -24,23 +24,22 @@ def do_test (_, name, xmltype):
           # Test output (results) data...
         testpgi = '%s/%s.pgi' % (Workdir, name)
         testlog = '%s/%s.log' % (Workdir, name)
-
         run = lambda cmd: subprocess.run (cmd, shell=True, check=True)
+
         opts = "-o %s %s %s" % (testxml,xmltype,testdata)
         run ("../tools/jmbuild.py " + opts)
         opts = "-p -o %s -l %s %s" % (testpgi,testlog,testxml)
         run ("../bin/jmparse.py " + opts)
+
         with open (expectpgi) as f: expected = f.read()
         with open (testpgi) as f: got = f.read()
-        if expected == got: diff = ''
-        else: diff = diff_strings (expected, got)
-        _.assertFalse (diff, msg=diff)
+        _.assertEqual (expected, got)
 
         with open (expectlog) as f: expected = f.read()
         with open (testlog) as f: got = f.read()
-        if expected == got: diff = ''
-        else: diff = diff_strings (expected, got)
-        _.assertFalse (diff, msg=diff)
+        got = re.sub (r'^[0-9]{6}-[0-9]{6}-[0-9]+',
+                      '000000-000000-0000', got)
+        _.assertEqual (expected, got)
 
 def runcmd (cmdln):
         proc = subprocess.Popen (cmdln, shell=True, env=os.environ,
@@ -58,19 +57,6 @@ def mk_temp_dir (keep=False):
 def rm_temp_dir (dirname):
         print ("Removing temp directory:", dirname)
         shutil.rmtree (dirname)
-
-def diff_strings (a, b):
-        """Return ndiff between two strings containing lines.
-        A trailing newline is added if missing to make the strings
-        print properly."""
-
-        if b and b[-1] != '\n': b += '\n'
-        if a and a[-1] != '\n': a += '\n'
-        #difflines = difflib.ndiff(a.splitlines(True), b.splitlines(True),
-        #             linejunk=lambda x: False, charjunk=lambda x: False)
-        difflines = difflib.unified_diff (a.splitlines(True),
-                                          b.splitlines(True), n=0)
-        return ''.join(difflines)
 
 if __name__ == '__main__':
         global_setup ()