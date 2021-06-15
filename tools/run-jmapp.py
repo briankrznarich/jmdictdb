df2bc7cd195b74c3a9c6fa1ff55050a877887b8f
Stuart McGraw <stuart@soga.home>
2021-06-15 14:55:59 -0600

    jmapp: run-jmapp.py: fixed to set JMAPP_CFG before jmapp import
    Setting the JMAPP_CFG environment variable before importing
    jmdictdb/jmapp.py is required by rev 210614-d6e6fc3.  Comments
    in that commit saying this had been done for run-jmapp.py were
    wrong it seems.

--- tools/run-jmapp.py
+++ tools/run-jmapp.py
@@ -6,30 +6,39 @@
 #
 # If an argument is given, it is name of the config file to be used.
 # Otherwise, if the environment variable JMAPP_CFGFILE is defined, it
-# is used as the config file.  Relative filenames are relative to the
-# current directory as usual.  If neither of those case hold, the config
-# file used will be "../jmdictdb/lib/cfgapp.ini", relative to this file's
-# location.
+# is used as the config file.  A relative filename given on the command
+# line is relative to the current working directory; a filename given
+# by environment variable is relative to the location of this file.
+# If the config file name is given by either command line or environment
+# variable it will default to  '../web/lib/cfgapp.ini' relative to this
+# file's location.
 
 import sys, os, pdb
 _=sys.path; _[0]=_[0]+('/' if _[0] else '')+'..'
+p = os.path     # For brevity
 
-from jmdictdb import jmapp
-App = jmapp.App
-
-DEFAULT_CFGFILE = '../jmdictdb/lib/cfgapp.ini'  # Relative to our directory.
+  # Following should be defined relative to run-jmapp.py's directory.
+DEFAULT_CFGFILE = '../web/lib/cfgapp.ini'
 
 def main():
-        p = os.path      # For brevity.
-        if len (sys.argv) > 1: cfgfile = sys.argv[1]
+        if len (sys.argv) > 1: cfgfile = abspath (sys.argv[1])
         else: cfgfile = os.environ.get('JMAPP_CFGFILE')
-        if not cfgfile:
-            d = p.dirname (__file__)
-            cfgfile = p.normpath (p.join (d, DEFAULT_CFGFILE))
+        if not cfgfile: cfgfile = DEFAULT_CFGFILE
+        cfgfile = abspath (cfgfile, p.dirname (__file__))
         print ("Using cfgfile: %s" % cfgfile, file=sys.stderr)
         os.environ['JMAPP_CFGFILE'] = cfgfile
+          # The jmdictdb.jmapp import MUST be done after JMAPP_CFGFILE is set.
+        from jmdictdb import jmapp
+        App = jmapp.App
         App.jinja_env.auto_reload = True
         App.config['TEMPLATES_AUTO_RELOAD'] = True
         App.run (host='0.0.0.0', debug=True)
 
+  #	FIXME: This function is a duplicate of config.abspath()
+def abspath (path, relative_to=None):
+          # Like os.path.abspath() but allows 'path' to be relative
+          # to an arbitrary directory.
+        if not relative_to: relative_to = os.getcwd()
+        fn = p.normpath (p.join (p.abspath (relative_to), path))
+        return fn
 main()
