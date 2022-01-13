1e2f4bd8c74bce6d27ecbfcf2e272155fbf56e02
Stuart McGraw <stuart@fuji.home>
2022-01-13 00:09:47 -0700

    tools/dbg-parser.py: add readline support

--- tools/dbg-parser.py
+++ tools/dbg-parser.py
@@ -5,7 +5,7 @@
 # Run the JEL lexer or parser using input read from stdin
 # and print the results.
 
-import sys, pdb
+import sys, readline, pdb
 _=sys.path; _[0]=_[0]+('/' if _[0] else '')+'..'
 from jmdictdb import logger; from jmdictdb.logger import L
 from jmdictdb import jdb, jelparse, jellex
@@ -13,21 +13,26 @@ from jmdictdb import jdb, jelparse, jellex
 def main():
         args = parse_cmdline()
         logger.log_config (level=args.level)
-        jdb.dbOpen (args.database)   # Initializes jdb.KW which is needed
-                                     #  by the lexer and parser.
+          # Initialize jdb.KW which is needed by the lexer and parser.
+        if not args.database: jdb.KW = jdb.Kwds('')
+        else: jdb.dbOpen (args.database)
         if args.jel:
             run (args.action, args.jel, args.trace, args.brk)
             return
+        prompt="Enter text (can be multi-line), ^D to parse, ^C to exit\n"
+        intxt, actv_prompt = '', prompt
         while True:
-            print ("Enter text (can be multi-line), ^D to parse, ^C to exit")
-            try: intxt = sys.stdin.read()
+            try: intxt += input (prompt) + '\n'
             except KeyboardInterrupt: break
-            run (args.action, intxt, args.trace, args.brk)
+            except EOFError:
+                run (args.action, intxt, args.trace, args.brk)
+                intxt, actv_prompt = '', prompt
+            else: prompt = ''
         print()
 
-def run (action, intxt, trace, brk):  
+def run (action, intxt, trace, brk):
         if not intxt: return
-        intxt = ffify (intxt)
+        intxt = ffify (intxt.rstrip('\n'))
         if action == 'lex':       # Run lexer
             lex (intxt)
         elif action == 'parse':   # Run parser
@@ -35,8 +40,8 @@ def run (action, intxt, trace, brk):
             except Exception as excp:
                 print (excp)
                 if hasattr (excp, 'loc'):
-                    print (excp.loc.replace('\f','')) 
-            else: 
+                    print (excp.loc.replace('\f',''))
+            else:
                 print (entr)
                 if brk: pdb.set_trace()
         else: raise ValueError ("Bad 'action' value")
@@ -66,15 +71,15 @@ def ffify (intxt):
         # the kanji, reading , and sense sections to be separated with
         # form-feed characters, not \n's as previously.  Because
         # form-feed characters are hard to enter interactively, we
-        # allow the two character sequence "\f" or "^L" and replace
-        # any occurances of them with real formfeeds ("\f").
+        # allow the two character sequence "\\f" and replace any
+        # occurances of them with real formfeeds ("\f").
         # If there are no formfeeds in 'intxt' at all (after the
         # above replacements), we replace the first two \n's with
         # formfeeds.
         #FIXME: replacing first two '\n's doesn't work with the
-        # experimental "extended jel" metainfo proposal since the 
+        # experimental "extended jel" metainfo proposal since the
         # metainfo preceeds the reading/kanji text and can have
-        # newline characters in it.  
+        # newline characters in it.
 
         if "\\f" in intxt: intxt = intxt.replace ("\\f", "\f")
         if "\f" in intxt: return intxt
@@ -98,7 +103,7 @@ def parse_cmdline():
             'using form-feed characters to separate the reading, kanji '
             'and senses sections.  If the two character sequence "\\f" '
             'or the two character sequence "^L" are entered, they will '
-            'be converted to a form-feed character.  ' 
+            'be converted to a form-feed character.  '
             'Type ^D when input in complete.  Type ^C to exit.')
         p.add_argument ('action', choices=['lex','parse'],
             help="Action to perform.")
@@ -106,7 +111,7 @@ def parse_cmdline():
             help="JEL text to lex or parse.  If not given you will be "
                 "prompted to enter the JEL text.")
         p.add_argument ('--break', '-b',
-            dest='brk',  # "break" is a Python reserved word. 
+            dest='brk',  # "break" is a Python reserved word.
             default=False, action='store_true',
             help="Start the Python debugger after a successful parse "
                 "to allow detailed examination of the resulting Entr "
@@ -117,9 +122,10 @@ def parse_cmdline():
         p.add_argument ('--trace', '-t', default=False, action='store_true',
             help="Print parser actions by running the parse with Ply's "
                 "debug option.  Has no effect when action is \"lex\".")
-        p.add_argument ('--database', '-d', default='jmdict',
+        p.add_argument ('--database', '-d', default=None,
             help="URL for a JMdictDB database that will be used as "\
-                "the source for keyword values.  Default is \"jmdict\".")
+                "the source for keyword values.  If not given, keywords "
+                "will be loaded from the CSV files.")
         args = p.parse_args ()
         return args
 
